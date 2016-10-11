f0dbf0ad8667133bfdb574dd27697228ca5f7dea
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-11 15:53:09 +0530

    Updated layout

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Introduction extends Component {
   render() {
     return(
-      <div><h1>Introduction</h1></div>
+      <div>
+        <h1>Introduction</h1>
+        <p>In this codelabs, you will be learning what is a Progressive Web Application and how it works.</p>
+      </div>
     );
   }
 }
--- src/PWA.js
+++ src/PWA.js
@@ -3,8 +3,17 @@ import React, {Component} from 'react';
 class PWA extends Component {
   render() {
     return(
-      <div>
+      <div className="pwa">
         <h1>Progressive Web Application</h1>
+        <p>So, what are progressive web applications ?</p>
+        <ul>
+          <li>Loads instantly regardless of network.</li>
+          <li>Secure, https by default.</li>
+          <li>Works when offline.</li>
+          <li>Add to homescreen.</li>
+          <li>Mobile friendly.</li>
+          <li>Works across the devices and browsers.</li>
+        </ul>
       </div>
     );
   }
