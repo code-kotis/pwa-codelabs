719cde1d9294f2e6462390f2a713cfce47508825
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-12 08:38:43 +0530

    Adding meta to sections, draft 1

--- src/Introduction.js
+++ src/Introduction.js
@@ -5,7 +5,15 @@ class Introduction extends Component {
     return(
       <div>
         <h1>Introduction</h1>
-        <p>In this codelabs, you will be learning what is a Progressive Web Application and how it works.</p>
+        <p>In this codelabs, you will be learning what is a Progressive Web Application and how it works and how to build one.</p>
+        <p className="mtop30"><b>Things you will learn</b></p>
+        <ul>
+          <li>What is a progressive web application.</li>
+          <li>Service worker lifecycle.</li>
+          <li>How to cache the resources.</li>
+          <li>Offline.</li>
+          <li>Push notifications.</li>
+        </ul>
       </div>
     );
   }
--- src/PWA.js
+++ src/PWA.js
@@ -10,9 +10,17 @@ class PWA extends Component {
           <li>Loads instantly regardless of network.</li>
           <li>Secure, https by default.</li>
           <li>Works when offline.</li>
-          <li>Add to homescreen.</li>
+          <li>Add to homescreen, Splash screen.</li>
           <li>Mobile friendly.</li>
           <li>Works across the devices and browsers.</li>
+        </ul>
+
+        <p><b>Examples:</b></p>
+        <ul className="disc">
+          <li><a href="https://airhorner.com/" target="_blank" rel="noopener">Air horner</a></li>
+          <li><a href="https://qrcodescan.in" target="_blank" rel="noopener">QR Code scanner</a></li>
+          <li><a href="https://jsfeatures.in" target="_blank" rel="noopener">JS Features</a></li>
+          <li><a href="http://demopwa.in" target="_blank" rel="noopener">Demo PWA</a></li>
         </ul>
       </div>
     );
