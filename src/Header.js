0c46d5fcd3e6f3472582ac33998fdf7c30167cb4
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-16 13:22:40 +0530

    Updated header

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
             <path d="M6 36h36v-4H6v4zm0-10h36v-4H6v4zm0-14v4h36v-4H6z"></path>
           </svg>
         </div>
-        <h3>Progressive Web Application</h3>
+        <h3>Codelabs</h3>
       </header>
     );
   }
