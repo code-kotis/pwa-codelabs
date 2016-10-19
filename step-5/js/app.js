f24f3ea466803fa6112eaa5517a0aeb5aeeb0823
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-19 09:22:03 +0530

    Updated step-5

---
+++ step-5/js/app.js
@@ -0,0 +1,37 @@
+(function () {
+
+	var githubCard = document.querySelector(".github__card");
+	var avatarElement = githubCard.querySelector(".github__header img");
+	var profileLinkElement = githubCard.querySelector(".github__header a");
+	var githubBodyElement = githubCard.querySelector(".github__body-content");
+	var nameElement = githubBodyElement.querySelector(".name span");
+	var bioElement = githubBodyElement.querySelector(".bio .text");
+	var locationElement = githubBodyElement.querySelector(".location span");
+	var githubFooterElement = githubCard.querySelector(".github__footer-content");
+	var repoElement = githubFooterElement.querySelector(".repos .text");
+	var followersElement = githubFooterElement.querySelector(".followers .text");
+	var followingElement = githubFooterElement.querySelector(".following .text");
+
+	fetch('https://api.github.com/users/gokulkrishh')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			if (data && data.message) {
+				return false;
+			}
+			avatarElement.src = data.avatar_url;
+			nameElement.textContent = data.name;
+			bioElement.textContent = data.bio;
+			locationElement.textContent = data.location;
+			profileLinkElement.href = data.html_url;
+			repoElement.textContent = data.public_repos;
+			followersElement.textContent = data.followers;
+			followingElement.textContent = data.following;
+			githubFooterElement.classList.remove("hide");
+			githubBodyElement.classList.remove("hide");
+		})
+		.catch(function (error) {
+			console.error("Error in fetching data ", error);
+		});
+})();
---
+++ step-5/service-worker.js
@@ -0,0 +1,78 @@
+var cacheName = 'cache-v3';
+
+var filesToCache = [
+	'./index.html',
+	'./index.html?utm=homescreen', //Query strings are treated as seperate page
+	'./css/styles.css',
+  './images/location.png',
+	'./images/fad_add.png',
+  './js/menu.js',
+	'./js/app.js',
+	'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' //3rd party resource
+];
+
+//Adding 'install' event listener
+self.addEventListener('install', function (event) {
+  console.log('Event: Install');
+
+  event.waitUntil(
+  	//Open the cache
+  	caches.open(cacheName)
+  		.then(function (cache) {
+  			//Adding the files to cache
+  			return cache.addAll(filesToCache)
+  				.then(function () {
+  					console.log("All files are cached.");
+  				})
+  		})
+  		.catch(function (err) {
+  			console.log(err)
+  		})
+	);
+});
+
+//Adding 'activate' event listener
+self.addEventListener('activate', function (event) {
+  console.log('Event: Activate');
+
+  event.waitUntil( 
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cache) {
+          console.log(cache)
+          if (cache !== cacheName) {     //cacheName = 'cache-v1'
+            return caches.delete(cache); //Deleting the cache
+          }
+        })
+      );
+    })
+  );
+});
+
+//Adding 'fetch' event listener
+self.addEventListener('fetch', function (event) {
+  console.log('Event: Fetch - ', event.request.url);
+  
+  var request = event.request;
+  
+  //Tell the browser to wait for network request and respond with below
+  event.respondWith(
+    //If request is already in cache, return it
+    caches.match(request).then(function(response) {
+      if (response) {
+        return response;
+      }
+
+      //else add the request to cache and return the response
+      return fetch(request).then(function(response) {
+        var responseToCache = response.clone(); //Cloning the response stream in order to add it to cache
+        caches.open(cacheName).then(
+          function(cache) {
+            cache.put(request, responseToCache); //Adding to cache
+          });
+
+        return response;
+      });
+    })
+  );
+});
