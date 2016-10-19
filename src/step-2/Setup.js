aa7bb57c03bc56308f7c0c528d0ebd977e6dcfac
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-19 18:17:46 +0530

    Updated step-2

--- src/step-2/Setup.js
+++ src/step-2/Setup.js
@@ -6,6 +6,7 @@ class Setup extends Component {
     const gitClone = '$ git clone https://github.com/code-kotis/pwa-codelabs.git';
     const switchBranch = '$ git checkout sample';
     const installDependencies = '$ npm install';
+    const runProject = '$ npm run server';
     return(
       <div>
         <h1>2. Setup</h1>
@@ -24,9 +25,13 @@ class Setup extends Component {
           
           <a className="btn" href="https://nodejs.org/dist/v4.6.1/node-v4.6.1.pkg" target="_blank">Download Node</a>
 
-          <li>Final step, install the dependencies to run localhost server.</li>
+          <li>Install the dependencies to run localhost server.</li>
           
           <Highlight lang="bash" value={installDependencies} />
+
+          <li>Final step. To run the sample repo. Below command will open the localhost in your browser.</li>
+          
+          <Highlight lang="bash" value={runProject} />
         </ul>
 
         <p>That's it. Setup is done.</p>
