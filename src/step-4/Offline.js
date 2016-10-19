f57c17d22bb7d1f8ec385d49366eb612faf34ce1
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-19 21:45:37 +0530

    Updated step-4

--- src/step-4/Offline.js
+++ src/step-4/Offline.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import appShell from './app-shell.png';
 
 class Offline extends Component {
   render() {
     return(
-      <div><h1>Offline</h1>
-      	<p></p>
+      <div className="offline">
+      	<h1>Offline</h1>
+      	<p>Making an application to work even when its offline is powerful feature.</p>
+
+      	<h2>App Shell Architecture</h2>
+      	<p>An app shell architecture is minimal level HTML, CSS, Javascript required to show the application user interface. By caching the app shell, application loads fast and repeated visits loads even faster.</p>
+      	<div className="offline__container">
+      		<img src={appShell} alt="app shell" />
+      	</div>
       </div>
     );
   }
