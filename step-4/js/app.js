6a9248739e1aecefd86c91c12e864c47b76415e3
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-19 08:36:55 +0530

    Added step-4

--- step-4/js/app.js
+++ step-4/js/app.js
@@ -2,18 +2,23 @@
 
 	var githubCard = document.querySelector(".github__card");
 	var avatarElement = githubCard.querySelector(".github__header img");
-	var nameElement = githubCard.querySelector(".name span");
-	var bioElement = githubCard.querySelector(".bio .text");
-	var locationElement = githubCard.querySelector(".location span");
-	var repoElement = githubCard.querySelector(".repos .text");
-	var followersElement = githubCard.querySelector(".followers .text");
-	var followingElement = githubCard.querySelector(".following .text");
+	var githubBodyElement = githubCard.querySelector(".github__body-content");
+	var nameElement = githubBodyElement.querySelector(".name span");
+	var bioElement = githubBodyElement.querySelector(".bio .text");
+	var locationElement = githubBodyElement.querySelector(".location span");
+	var githubFooterElement = githubCard.querySelector(".github__footer-content");
+	var repoElement = githubFooterElement.querySelector(".repos .text");
+	var followersElement = githubFooterElement.querySelector(".followers .text");
+	var followingElement = githubFooterElement.querySelector(".following .text");
 
 	fetch('https://api.github.com/users/gokulkrishh')
-		.then(function (response){
-			return response.json()
+		.then(function (response) {
+			return response.json();
 		})
 		.then(function (data) {
+			if (data && data.message) {
+				return false;
+			}
 			avatarElement.src = data.avatar_url;
 			nameElement.textContent = data.name;
 			bioElement.textContent = data.bio;
@@ -21,6 +26,8 @@
 			repoElement.textContent = data.public_repos;
 			followersElement.textContent = data.followers;
 			followingElement.textContent = data.following;
+			githubFooterElement.classList.remove("hide");
+			githubBodyElement.classList.remove("hide");
 		})
 		.catch(function (error) {
 			console.error("Error in fetching data ", error);
