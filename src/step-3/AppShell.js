fea6de2741bfb93d90d23fcd9701ec8de6452b8f
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-20 22:17:22 +0530

    Updated step-1 & final step

--- src/step-3/AppShell.js
+++ src/step-3/AppShell.js
@@ -6,17 +6,16 @@ class AppShell extends Component {
   render() {
     return(
       <div className="appshell">
-      	<h1>What is App Shell Architecture?</h1>
-      	<p>An app shell architecture is minimal level <span className="highlight bold no--bg">HTML, CSS, Javascript</span> required to show the application user interface.</p>
-        
-        <i>Note: To save some coding time, App shell is already added to the codelabs sample repository.</i>
-
-      	<div className="offline__container">
+      	<h1>App Shell Architecture</h1>
+      	<p>An app shell architecture is minimal level <span className="highlight bold no--bg">HTML, CSS, Javascript</span> required to power the application user interface. Cached app shell loads faster on the repeated visits and only the content is loaded dymanically.</p>
+        <div className="offline__container">
       		<img src={appShellImg} alt="app shell" />
       	</div>
+
+        <i>To save some coding time, App shell is already added to the codelabs sample repository of step 2.</i>
  
       	<Note type="facts">
-          <p><span>Facts: </span> Housing.com used app shell architecture in their <a href="housing.com" target="_blank">progressive web app</a> to load the page 30% faster than before. Read thier full <a href="https://developers.google.com/web/showcase/2016/pdfs/housing.pdf" target="_blank">case study</a>.</p>
+          <p><span>Facts: </span> Using app shell model, <a href="housing.com" target="_blank">Housing.com</a> were able to load the page 30% faster than before. Read thier full <a href="https://developers.google.com/web/showcase/2016/pdfs/housing.pdf" target="_blank">case study</a>.</p>
         </Note>
       </div>
     );
--- src/step-7/Finish.js
+++ src/step-7/Finish.js
@@ -7,11 +7,12 @@ class Finish extends Component {
 		const surgeInstall = `$ npm install --global surge`;
 		const finalDir = `$ cd final`;
 		const surgeDeploy = `$ surge`;
+		const surgeDeployHTTPS = `$ surge --domain https://my-project.surge.sh`;
     return(
       <div>
         <h1>Deploy</h1>
         <p>The final step of this codelabs is deploying our github cards with HTTPS server.</p>
-        <h3>Follow the below 3 steps to deploy</h3>
+        <h3>Follow the below steps.</h3>
 				<p><span className="highlight bold no--bg">Step 1</span> - Install surge via npm.</p>
 				<Highlight lang="bash" value={surgeInstall} />
 
@@ -26,6 +27,13 @@ class Finish extends Component {
 				<Note type="tips">
 					<p><span>Tips: </span> <a href="https://surge.sh/help/getting-started-with-surge" target="_blank">Surge</a> is static web publishing services. They provide free account with unlimited deployments, custom domain, basic SSL certificate.</p>
 				</Note>
+
+				<h3>What next?</h3>
+				<p>Everything is perfect, except the deployed site is loading in HTTP unless we change the url to HTTPS. So lets fix it by forcing HTTP to redirect to HTTPS.</p>
+					
+				<Highlight lang="bash" value={surgeDeployHTTPS} />
+
+				<p>Thats it all done!!</p>
       </div>
     );
   }
